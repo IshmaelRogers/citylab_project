de2bbcc3e9a38b4749b4e715dc9c5d033312d27e
Ishmael <ishmael@ishr.dev>
2024-12-27 19:06:27 +0000

    outlined the class for direction service

---
+++ robot_patrol/src/direction_service.cpp
@@ -0,0 +1,18 @@
+#include "rclcpp/node.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "robot_patrol/srv/get_directions.hpp"
+
+class DirectionService : public rclcpp::Node
+{
+
+
+
+};
+
+int main(int argc, char **argv)
+{
+    return 0;
+
+
+}
